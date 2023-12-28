eae3c2370db0c0b696d845ce263529dcfe0626ca
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-28 17:18:46 +0200

    create serializer for Crew and view for model Crew, also add endpoint for view

--- airport/urls.py
+++ airport/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import CrewViewSet
+
+router = routers.SimpleRouter()
+router.register("crews", CrewViewSet)
 
 urlpatterns = [
+    path("", include(router.urls))
 ]
 
 app_name = "airport"
