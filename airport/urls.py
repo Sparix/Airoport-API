a6135a332a19719f50b0dc7a51f469c1872b4b8e
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-28 17:24:22 +0200

    add serializer, view and endpoint for model AirplaneType

--- airport/urls.py
+++ airport/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import CrewViewSet
+from airport.views import CrewViewSet, AirplaneTypeViewSet
 
 router = routers.SimpleRouter()
 router.register("crews", CrewViewSet)
+router.register("airplane_type", AirplaneTypeViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
--- airport/views.py
+++ airport/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from airport.models import Crew
-from airport.serializers import CrewSerializer
+from airport.models import Crew, AirplaneType
+from airport.serializers import CrewSerializer, AirplaneTypeSerializer
 
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
