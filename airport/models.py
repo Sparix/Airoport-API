b0eecfe27a0815d30b4c87d6634f117bfcdb807f
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-28 13:52:19 +0200

    add models Flight, Route, Airport

--- airport/models.py
+++ airport/models.py
@@ -37,3 +37,48 @@ class Airplane(models.Model):
             f"Capacity: {self.capacity}"
             f"Airplane Type: {self.airplane_type}"
         )
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return (
+            f"Source: {self.source.name}"
+            f"Destination: {self.destination.name}"
+        )
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="flights")
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, related_name="flights")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew, blank=True)
+
+    def __str__(self):
+        return (
+            f"Source: {self.route.source.name}"
+            f"Destination: {self.route.destination.name}"
+        )
+
+    class Meta:
+        ordering = ("departure_time", "arrival_time",)
