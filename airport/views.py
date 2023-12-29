dffab2d4519eda03732634c2b6df6dda7552eceb
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-29 10:07:46 +0200

    create viewset for models Airplane, Airport, Route, Flight

--- airport/views.py
+++ airport/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 
-from airport.models import Crew, AirplaneType
-from airport.serializers import CrewSerializer, AirplaneTypeSerializer
+from airport.models import Crew, AirplaneType, Airplane, Airport, Route, Flight
+from airport.serializers import (
+    CrewSerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer, RouteSerializer, AirportSerializer, FlightSerializer
+)
 
 
 class CrewViewSet(viewsets.ModelViewSet):
@@ -12,3 +16,23 @@ class CrewViewSet(viewsets.ModelViewSet):
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.select_related("airplane_type")
+    serializer_class = AirplaneSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related("source", "destination")
+    serializer_class = RouteSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.select_related("route", "airplane").prefetch_related("crew")
+    serializer_class = FlightSerializer
