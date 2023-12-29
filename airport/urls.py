77f064f465d0705c5761eab2b64b21a74b6d49f6
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-29 10:10:29 +0200

    create endpoint for view: Airport, Airplane, Route, Flight

--- airport/urls.py
+++ airport/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import CrewViewSet, AirplaneTypeViewSet
+from airport.views import (
+    CrewViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    AirportViewSet,
+    FlightViewSet,
+    RouteViewSet,
+)
 
 router = routers.SimpleRouter()
 router.register("crews", CrewViewSet)
-router.register("airplane_type", AirplaneTypeViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("flights", FlightViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
