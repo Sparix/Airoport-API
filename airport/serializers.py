f594f062c656819f362199afd2f5f69b0befd4c8
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-29 09:39:18 +0200

    create serializers for model Airplane, Airport

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from airport.models import Crew, AirplaneType
+from airport.models import Crew, AirplaneType, Airplane, Airport
 
 
 class CrewSerializer(serializers.ModelSerializer):
@@ -13,3 +13,24 @@ class AirplaneTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
         fields = ("id", "name",)
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    airplane_type = AirplaneTypeSerializer(read_only=True)
+
+    class Meta:
+        model = Airplane
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_row",
+            "capacity",
+            "airplane_type",
+        )
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "closest_big_city",)
