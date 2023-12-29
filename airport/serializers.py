cf851b3213cd6895468f3b2484b6c358291e6618
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-29 09:56:59 +0200

    add serializers for model Route, Flight

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from airport.models import Crew, AirplaneType, Airplane, Airport
+from airport.models import Crew, AirplaneType, Airplane, Airport, Route, Flight
 
 
 class CrewSerializer(serializers.ModelSerializer):
@@ -34,3 +34,22 @@ class AirportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airport
         fields = ("id", "name", "closest_big_city",)
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    source = AirportSerializer(read_only=True)
+    destination = AirportSerializer(read_only=True)
+
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination", "distance",)
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    route = RouteSerializer(read_only=True)
+    airplane = AirplaneSerializer(read_only=True)
+    crew = CrewSerializer(read_only=True)
+
+    class Meta:
+        model = Flight
+        fields = ("id", "route", "airplane", "departure_time", "arrival_time", "crew",)
