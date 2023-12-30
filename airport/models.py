16aca4a578cce34498c355d02860b62b53a6fd89
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-30 13:31:17 +0200

    create models Order, Ticket and User, also add AUTH_USER_MODEl in settings

--- airport/models.py
+++ airport/models.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.db import models
 
 
-# Create your models here.
 class Crew(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -53,7 +53,7 @@ class Airport(models.Model):
         return self.name
 
     class Meta:
-        ordering = ("name", )
+        ordering = ("name",)
 
 
 class Route(models.Model):
@@ -91,3 +91,23 @@ class Flight(models.Model):
 
     class Meta:
         ordering = ("-departure_time", "-arrival_time",)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ("-created_at", )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
