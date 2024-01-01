1f790ab8253424d283cee35437e0d6a54724b62e
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-01 13:21:15 +0200

    create Token Authenticated, add login, and profile user

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView, ProfileUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("profile/", ProfileUserView.as_view(), name="profile"),
 ]
 
 app_name = "user"
