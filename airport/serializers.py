bb302f3dff60f3bf670600c4b1bfdbc1b2f69cc3
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-02 12:30:08 +0200

    add to OrderSerializer atomic transaction, create OrderListSerializer and TicketListSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -84,12 +85,32 @@ class TicketSerializer(serializers.ModelSerializer):
         return data
 
     class Meta:
+        model = Ticket
         fields = ("id", "row", "seat", "flight",)
+
+
+class TicketListSerializer(TicketSerializer):
+    flight = FlightListSerializer(many=False, read_only=True)
 
 
 class OrderSerializer(serializers.ModelSerializer):
     tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    created_at = serializers.DateTimeField(
+        format="%Y-%m-%d %H:%M:%S", read_only=True
+    )
 
     class Meta:
         model = Order
         fields = ("id", "tickets", "created_at")
+
+    @transaction.atomic()
+    def create(self, validated_data):
+        tickets_data = validated_data.pop("tickets")
+        order = Order.objects.create(**validated_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(order=order, **ticket_data)
+        return order
+
+
+class OrderListSerializer(OrderSerializer):
+    tickets = TicketListSerializer(many=True, read_only=True)
