559737d1b9709975282ac5c7a57d72c89c8c2281
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-02 10:51:23 +0200

    create FlightListSerializer, and OrderSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from airport.models import Crew, AirplaneType, Airplane, Airport, Route, Flight, Ticket
+from airport.models import (
+    Crew,
+    AirplaneType,
+    Airplane,
+    Airport,
+    Route,
+    Flight,
+    Ticket, Order
+)
 
 
 class CrewSerializer(serializers.ModelSerializer):
@@ -40,7 +48,6 @@ class AirportSerializer(serializers.ModelSerializer):
 
 
 class RouteSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Route
         fields = ("id", "source", "destination", "distance",)
@@ -52,13 +59,17 @@ class RouteListSerializer(RouteSerializer):
 
 
 class FlightSerializer(serializers.ModelSerializer):
-    route = RouteSerializer(read_only=True)
-    airplane = AirplaneSerializer(read_only=True)
-    crew = CrewSerializer(many=True)
-
     class Meta:
         model = Flight
         fields = ("id", "route", "airplane", "departure_time", "arrival_time", "crew",)
+
+
+class FlightListSerializer(FlightSerializer):
+    route = RouteListSerializer(read_only=True)
+    airplane = AirplaneListSerializer(read_only=True)
+    crew = CrewSerializer(many=True)
+    departure_time = serializers.DateTimeField("%d-%h-%Y %H-%M")
+    arrival_time = serializers.DateTimeField("%d-%h-%Y %H-%M")
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -74,3 +85,11 @@ class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = ("id", "row", "seat", "flight",)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
