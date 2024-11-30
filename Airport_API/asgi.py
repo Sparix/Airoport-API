660daa0395fb562f693169bb55ae700582587ce9
Oleksandr Hontarenko <sparix@Mac.best.net>
2024-11-30 16:04:38 +0200

    configure asgi and add CHANNEL_LAYERS for redis

--- Airport_API/asgi.py
+++ Airport_API/asgi.py
@@ -9,8 +9,24 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat_msg.jwt_middleware import JWTAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Airport_API.settings")
 
-application = get_asgi_application()
+asgi_app = get_asgi_application()
+
+from chat_msg import routing
+
+application = ProtocolTypeRouter({
+    "http": asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(
+                routing.websocket_urlpatterns
+        )
+    ))
+})
